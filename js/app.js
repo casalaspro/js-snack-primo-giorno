e193679c2e010260df9c80051d64c47b6fa32eba
casalaspro <casalaspro.alessandro@gmail.com>
2024-03-01 13:22:42 +0100

    ADD - index, css, js

---
+++ js/app.js
@@ -0,0 +1,46 @@
+/*
+Esercizio 1
+Dichiara una variabile chiamata number e assegnale il valore 100. Stampa il valore della variabile in console.
+*/
+
+let number = 100; //number
+console.log("La variabile number ha valore ", number);
+
+/*
+Esercizio 2
+Dichiara una costante chiamata PI e assegnale il valore di 3.14. Stampa il valore della costante in console.
+*/
+
+const PI = 3.14;  //number
+console.log("La costante PI ha valore ", PI);
+
+/*
+Esercizio 2/B
+Prova ad assegnare alla costante chiamata PI il valore di 3.1416. Sai dire cosa succede?
+*/
+
+// PI = 3.1416; // la console restituisce un errore: non si può riassegnare un valore ad una costante.
+
+/*
+Esercizio 2/C
+Dichiara una variabile chiamata radius e assegnale il valore 8.
+Moltiplica radius x 2 e x PI e assegna il valore a una variabile di nome circle.
+Stampa il valore della variabile circle in console.
+*/
+
+let radius = 8;
+let circle = radius * 2 * PI;
+console.log("La variabile circle ha valore ", circle);
+
+/*
+Esercizio 3
+Dichiara una variabile chiamata name e assegnale il valore “Carlo”.
+Successivamente, modifica il valore della variabile in “Marco”.
+Stampa il nuovo valore in console.
+*/
+
+let name = "Carlo"; //string
+name = "Marco"  //string
+
+console.log(name);
+
